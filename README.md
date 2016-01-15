e5346836dc1f717a918821c7093c08fa5158c047
Adam Ross Russell <student@epicodus.com>
2016-01-15 11:57:15 -0800

    add readme info and added show sections to all but the else function

---
+++ README.md
@@ -0,0 +1,41 @@
+# Vacation Suggestion Generator
+
+#### Vacation Destination Generator, 1/15/2016
+
+#### By Adam Ross Russell
+
+## Description
+
+This website will generate vacation destinations fit to the user, based on the inputs they select on the form.
+
+## Setup/Installation Requirements
+
+* Sign in to Github, search for adamrr724
+* Find repository VacationProject
+* Fork and Zip/Clone repository
+* Open HTML file
+* Fill out form and submit
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+Please contact adamrr724@yahoo.com with any technical difficulties.
+
+## Technologies Used
+
+This webpage uses:
+
+HTML
+CSS
+Javascript
+JQuery
+Bootstrap
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2015 Adam Ross Russell
--- js/scripts.js
+++ js/scripts.js
@@ -12,24 +12,26 @@ var pictures = ['<img src="img/bahamas.jpg" />','<img src="img/orlando.jpg" />',
   if (weather === 'Warm' && activity === 'Beach Bum'){
     $('#generator').after(pictures[0]);
     $('#bahamas').show();
+    $('#generator').show();
   }
    else if (weather === 'Warm' && activity === 'Amusement Parks'){
      $('#generator').after(pictures[1]);
      $('#orlando').show();
+     $('#generator').show();
    }
    else if (weather === 'Mild' && activity === 'Hike in the Forest'){
      $('#generator').after(pictures[2]);
       $('#portland').show();
+      $('#generator').show();
    }
    else if (weather === 'Cold' && activity === 'Ski Bum'){
      $('#generator').after(pictures[3]);
       $('#vail').show();
+      $('#generator').show();
     }
    else {
      alert('Your answers are too different for us to make a proper assessment. For example, you may have chosen that you like to be a warm ski bum! This may be ideal, but not realistic for us to give you a good result. Please try again! :)')
    }
-
-   $('#generator').show();
 
     event.preventDefault();
   });
