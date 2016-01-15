1691140d450c8d335c2796b98283a3b6e2714aca
Adam Ross Russell <student@epicodus.com>
2016-01-15 14:43:15 -0800

    add styling, removal or bottom by clicking, and links

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,48 @@ var weather = $("select#weather").val();
 var terrain = $("select#terrain").val();
 var activity = $("select#activity").val();
 
-var pictures = ['<img src="img/bahamas.jpg" />','<img src="img/orlando.jpg" />', '<img src="img/portland.jpg" />','<img src="img/vail.jpg" />',];
+var pictures = ['<img class="img" src="img/bahamas.jpg" />','<img src="img/orlando.jpg" />', '<img src="img/portland.jpg" />','<img src="img/vail.jpg" />','<img src="img/paris.jpeg" />', ];
 
   if (weather === 'Warm' && activity === 'Beach Bum'){
     $('#generator').after(pictures[0]);
     $('#bahamas').show();
-    $('#generator').show();
+    $('.matches, #generator').show();
+      $ (".matches").click(function() {
+        $(this).hide();
+      });
   }
-  else if (weather === 'Warm' && activity === 'Amusement Parks'){
+  else if (weather === 'Warm' && activity === 'Amusement Parks' || weather === 'Mild' && activity === 'Amusement Parks' ){
      $('#generator').after(pictures[1]);
      $('#orlando').show();
-     $('#generator').show();
+     $('.matches, #generator').show();
+       $ (".matches").click(function() {
+         $(this).hide();
+       });
    }
-  else if(weather === 'Mild' && activity === 'Hike in the Forest'){
+  else if(weather === 'Mild' && activity === 'Hike in the Forest' || weather === 'Cold' && activity === 'Hike in the Forest' || weather === 'Mild' && activity === 'Shopping' && terrain === 'Mountain'){
      $('#generator').after(pictures[2]);
       $('#portland').show();
-      $('#generator').show();
+      $('.matches, #generator').show();
+        $ (".matches").click(function() {
+          $(this).hide();
+        });
    }
-  else if (weather === 'Cold' && activity === 'Ski Bum'){
+  else if (weather === 'Cold' && activity === 'Ski Bum' || weather === 'Mild' && activity === 'Ski Bum' || weather === 'Cold' && activity === 'Shopping'){
      $('#generator').after(pictures[3]);
       $('#vail').show();
-      $('#generator').show();
+      $('.matches, #generator').show();
+        $ (".matches").click(function() {
+          $(this).hide();
+        });
     }
+  else if (weather === 'Mild' && activity === 'Shopping' || weather === 'Warm' && activity === 'Shopping') {
+      $('#generator').after(pictures[4]);
+       $('#paris').show();
+       $('.matches, #generator').show();
+         $ (".matches").click(function() {
+           $(this).hide();
+         });
+       }
    else {
      alert('Your answers are too different for us to make a proper assessment. For example, you may have chosen that you like to be a warm ski bum! This may be ideal, but not realistic for us to give you a good result. Please try again! :)')
    }
